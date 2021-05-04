aff7136b715fd178caab0bbb34a495a4e09ef51d
Mucio M G Junior <mlk_mucio@hotmail.com>
2021-05-04 15:10:24 -0300

    Componentes básicos criados

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
 
 
 function App() {
   return (
-    <div >
-      <h1 className="text-primary">Bem-vindo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Bem-vindo!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
